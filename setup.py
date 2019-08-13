080c3a1fecbc4e6d8a0a506e406d3e5b6c5836f1
lusob <luis.sobrecueva@careesma.com>
2019-08-13 14:00:55 +0200

    Added code of python3 port of google sitemap generator

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='sitemap_gen',
+      version='3',
+      description='Sitemap Generator',
+      license='BSD',
+      author='lusob',
+      author_email='luis.sobrecueva@gmail.com',
+      url='https://github.com/lusob/sitemap_gen'
+      platforms = ['POSIX', 'Windows'],
+      py_modules = ['sitemap_gen'],
+     )
